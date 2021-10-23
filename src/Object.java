8aa548800a3d1d6521ea1bf767be2e819125795c
kadekframa <dframa19@gmail.com>
2021-10-23 23:55:30 +0800

    Java OOP Trainning Code, belajar object di Java. Tetap Semangat!

---
+++ src/Object.java
@@ -0,0 +1,20 @@
+public class Object {
+    public static void main(String[] args) {
+        // Object di Java.
+        // Object adalah hasil instansiasi dari sebuah class.
+        // Untuk membuat object kita bisa menggunakan kata kunci new, dan diikuti dengan nama class dan kurung "()".
+
+        // Contoh penggunaan object.
+        var person1 = new Person();
+        var person2 = new Person();
+
+        Person person3;
+        person3 = new Person();
+
+        System.out.println(person1);
+        System.out.println(person2);
+        System.out.println(person3);
+
+    }
+
+}
