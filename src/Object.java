78f17440146a16cf6b2eaa3589fe578a76d261b1
kadekframa <dframa19@gmail.com>
2021-10-24 15:41:50 +0800

    Java OOP Trainning Code, belajar Field di Java. Tetap Semangat!!!

--- src/Object.java
+++ src/Object.java
@@ -5,16 +5,23 @@ public class Object {
         // Untuk membuat object kita bisa menggunakan kata kunci new, dan diikuti dengan nama class dan kurung "()".
 
         // Contoh penggunaan object.
-        var person1 = new Person();
+        var person1 = new Field();
+        person1.name = "Kadek";                     // Jadi cukup dengan menggunakan "." (titik) dan diikuti dengan nama field nya untuk dapat mengubah field dari suatu object.
+        person1.address = "Bali";
+//        person1.country = "Indonesia";  // error
+
+        System.out.println(person1.name);
+        System.out.println(person1.address);
+        System.out.println(person1.country);
 
         Person person2 = new Person();
 
         Person person3;
         person3 = new Person();
-
+/*
         System.out.println(person1);
         System.out.println(person2);
-        System.out.println(person3);
+        System.out.println(person3);*/
 
     }
 
