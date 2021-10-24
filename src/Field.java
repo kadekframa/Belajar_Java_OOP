f767ecdcb2814715325162b0fe7f603e453972fb
kadekframa <dframa19@gmail.com>
2021-10-24 15:33:01 +0800

    Java OOP Trainning Code, belajar Field di Java. Tetap Semangat!

---
+++ src/Field.java
@@ -0,0 +1,12 @@
+class Field {
+    // Field di Java.
+    // Field/ propertied/ atribut adalah data yang bisa kita sisipkan di dalam object.
+    // Namun sebelum kita bisa memasukkan data di fields, kita harus mendeklarasikan data apa saja yang dimiliki object tersebut di dalam deklarasi class-nya.
+    // Membuat field sama seperti membuat variable, namun ditempatkan di block class.
+
+    String name;
+    String address;
+    final String country = "Indonesia";
+
+
+}
