ce38781624864bb705c608c8105d85c1cc3dc685
kadekframa <dframa19@gmail.com>
2021-11-04 00:51:13 +0800

    Java OOP Trainning Code, belajar penggunaan inheritance di Java.

---
+++ src/Manajer.java
@@ -0,0 +1,15 @@
+class Manajer {
+    // Inheritance.
+    // Inheritance atau pewarisan adalah kemampuan untuk menurunkan sebuah class ke class lain.
+    // Dalam artian, kita bisa membuat class Parent dan class Child.
+    // Class Child, hanya punya satu class Parent, namun satu class Parent bisa punya banyak class Child.
+    // Saat sebuah class diturunkan, maka semua field dan method yang ada di class  Parent, secara otomatis akan dimiliki oleh class child.
+    // Untuk melakukan pewarisan, di class child, kita harus menggunakan kata kunci extends lalu diikuti dengan nama class parent nya.
+
+    String name;
+
+    void sayHello(String name){
+        System.out.println("Hello " + name + ", My name is " + this.name);
+    }
+
+}
---
+++ src/VicePresident.java
@@ -0,0 +1,3 @@
+class VicePresident extends Manajer{
+
+}
