be1ad580464bdb6aa900f250d756369500f07f07
kadekframa <dframa19@gmail.com>
2021-11-04 23:59:36 +0800

    Java OOP Trainning Code, belajar type check & casts di Java. Tetap Semangat...!!!

--- src/PolymorphismApp.java
+++ src/PolymorphismApp.java
@@ -19,7 +19,20 @@ public class PolymorphismApp {
 
     }
 
+    // Type check & casts
+    // Sebelumnya kita sudah tau cara melakukan konversi tipe data (casts) di tipe data primitif.
+    // Casts juga bisa digunakan untuk tipe data bukan primitif.
+    // Namun agar aman, sebelum melakukan casts, pastika kita melakukan tipe check (pengecekan tipe data), dengan menggunakan kata kunci intanceof.
+    // Hasil operator instanceof adalah boolean, true jika tipe data sesuai, false jika tidak sesuai.
+
     static void sayHello(Employee employee){
+        if(employee instanceof VicePresident){
+            VicePresident vicePresident = (VicePresident) employee;
+            System.out.println("Hello VP "  + vicePresident.name);
+        }else  if(employee instanceof  Manajer){
+            Manajer manajer = (Manajer) employee;
+            System.out.println("Hello Manajer " + employee.name);
+        }else
         System.out.println("Hello " + employee.name);
     }
 }
