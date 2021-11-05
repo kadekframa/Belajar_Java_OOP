6d58d58689c2cafeca1a786721c1fe8176ddaca2
kadekframa <dframa19@gmail.com>
2021-11-05 17:13:07 +0800

    Java OOP Trainning Code, learn about import in Java. Keep spirit dan Tetap Semangat...!!!

--- src/programmer/pemula/application/Application.java
+++ src/programmer/pemula/application/Application.java
@@ -1,6 +1,6 @@
 package programmer.pemula.application;
 
-import programmer.pemula.data.Produk;
+import programmer.pemula.data.*;        // Dengan menggunakan tanda bintang (*), kita dapat melakukan import semua class yang ada di packages programmer.pemula.data. Jadi lebih mudah untuk melakukan import.
 
 public class Application {
     public static void main(String[] args) {
@@ -8,6 +8,8 @@ public class Application {
         Produk produk = new Produk("macbook pro", 30000000);
         System.out.println(produk.name);
         System.out.println(produk.price);
+
+        Data data = new Data();
 
     }
 }
---
+++ src/programmer/pemula/data/Data.java
@@ -0,0 +1,5 @@
+package programmer.pemula.data;
+
+public class Data {
+
+}
