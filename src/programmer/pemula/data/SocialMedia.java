fb00fb903376825abd6ea7872e549d48909cbd5d
kadekframa <dframa19@gmail.com>
2021-11-07 17:20:41 +0800

    Java OOP Trainning Code, learn about final class in Java. Keep spirit, Tetap Semangat!!!

---
+++ src/programmer/pemula/data/SocialMedia.java
@@ -0,0 +1,13 @@
+package programmer.pemula.data;
+
+class SocialMedia {
+    String name;
+}
+
+final class  Facebook extends SocialMedia{
+
+}
+
+//class FakeFacebook extends Facebook{
+//    // ERROR
+//}
