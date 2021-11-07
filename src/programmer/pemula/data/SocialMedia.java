862bede6c584a0ea8e7fa069c317e33fa249e296
kadekframa <dframa19@gmail.com>
2021-11-07 19:34:28 +0800

    Java OOP Trainning Code, learn about final method in Java. Keep spirit, Tetap Semangat!!!

--- src/programmer/pemula/data/SocialMedia.java
+++ src/programmer/pemula/data/SocialMedia.java
@@ -4,10 +4,22 @@ class SocialMedia {
     String name;
 }
 
-final class  Facebook extends SocialMedia{
+
+class  Facebook extends SocialMedia{
+    // Final Method.
+    // Kata kunci final juga bisa digunakan di method.
+    // Jika sebuah method kita tambahkan kata kunci final, maka artinya method tersebut tidak bisa di override lagi di class child nya.
+    // Ini sangat cocok jika kita ingin mengunci implementasi dari sebuah method agar tidak bisa diubah lagi oleh class child nya.
+
+    final void login(String username, String password){     // final method.
+
+    }
 
 }
 
-//class FakeFacebook extends Facebook{
-//    // ERROR
-//}
+
+class FakeFacebook extends Facebook{
+//    void login(String username, String password){       // ERROR, method login tidak bisak di override di class childnya, karena method pada parent class sudah final method.
+//
+//    }
+}
