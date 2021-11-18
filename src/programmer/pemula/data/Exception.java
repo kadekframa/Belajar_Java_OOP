4f208ef179c768ec35bd7004d09e02b58cea977d
kadekframa <dframa19@gmail.com>
2021-11-18 21:37:32 +0800

    Java OOP Trainning Code, learn about exception in Java. Keep spirit dan Tetap Semangat Meraih Impian!!!

---
+++ src/programmer/pemula/data/Exception.java
@@ -0,0 +1,9 @@
+package programmer.pemula.data;
+
+public class Exception {
+    // Exception
+    // Saat kita membuat aplikasi, kita tidak akan terhindar dengan yang namanya error.
+    // Di java error di representasikan dengan istilah exception. dan semua direpresentasikan dalam bentuk class exception.
+
+
+}
