c8cf5b6b9344f924ffbf66a634dbbd16d492e8cc
kadekframa <dframa19@gmail.com>
2021-11-19 21:46:18 +0800

    Java OOP Trainning Code, learn about finally block exception in Java. Keep spirit dan Tetap Semangat Meraih Impian!!!

--- src/programmer/pemula/application/ValidationApp.java
+++ src/programmer/pemula/application/ValidationApp.java
@@ -11,6 +11,13 @@ public class ValidationApp {
         // Ini gunanya agar kita bisa menangkap exception yang terjadi, karena jika tidak ditangkap, lalu terjadi exception, maka secara otomatis program kita akan berhenti.
         // Cara menggunakan try-catch expression di java sangat mudah, di block try, kita tinggal panggil method yang bisa menyebabkan exception, dan di block catch, kita bisa melakukan sesuatu jika terjadi exception.
 
+
+        // Finally Keyword.
+        // Dalam try-catch, kita bisa menambahkan block finally.
+        // Block finally ini adalah block dimana akan selalu dieksekusi baik terjadi exception ataupun tidak.
+        // Ini sangat cocok ketika kita ingin melakukan sesuatu, tidak peduli sukses ataupun gagal, misal di blcok try kita ingin membaca file, di block catch kita akan tangkap jika terjadi error dan di block finally error ataupun sukses membaca file, kita wajib menutup koneksi ke file tersebut, biar tidak menggantung di memory.
+
+
         LoginRequest loginRequest = new LoginRequest("", "secret");
 
         try {                                           // membuat try-catch untuk mengatasi exception di Java. Try-catch ini berfungsi untuk menangkap exception yang kemungkinan bisa terjadi pada sebuah method.
@@ -18,6 +25,8 @@ public class ValidationApp {
             System.out.println("Data valid, Tetap Semangat");
         }catch (ValidationException | NullPointerException exception){              // Cara mudah dan simple untuk membuat multiple catch untuk menangkap exception.
             System.out.println("Telah terjadi error:" + exception.getMessage());
+        }finally {
+            System.out.println("Selalu dieksekusi !, Tetap Semangat !!!");
         }
 
 
