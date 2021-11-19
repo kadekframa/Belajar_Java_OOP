25c4af6de7156e6f4fb6abddba3292f298600424
kadekframa <dframa19@gmail.com>
2021-11-19 19:07:15 +0800

    Java OOP Trainning Code, learn about Exception in Java. Keep spirit dan Tetap Semangat Meraih Impian!!!

--- src/programmer/pemula/error/ValidationException.java
+++ src/programmer/pemula/error/ValidationException.java
@@ -1,4 +1,19 @@
 package programmer.pemula.error;
 
-public class ValidationException {
+public class ValidationException extends Throwable{
+    // Exception.
+    // Saat kita membuat aplikasi, kita tidak akan terhindar dengan yang namanya error.
+    // Di Java, error direpresentasikan dengan istilah exception, dan semua direpresentasikan dalam bentuk class exception.
+    // Kita bisa menggunakan class exception sendiri, atau menggunakan yang sudah disediakan oleh Java.
+    // Jika kita ingin membuat exception, maka kita harus membuat class yang extends class Throwable atau turunan-turunannya.
+
+    private String message;
+
+    public ValidationException(String message){
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
--- src/programmer/pemula/util/ValidationUtil.java
+++ src/programmer/pemula/util/ValidationUtil.java
@@ -1,4 +1,25 @@
 package programmer.pemula.util;
 
+import programmer.pemula.data.LoginRequest;
+import programmer.pemula.error.ValidationException;
+
 public class ValidationUtil {
+    // Membuat exception.
+    // Exception biasanya terjadi di method, ketika kita membuat exception di sebuah method, maka method tersebut harus ditandai dengan kata kunci throws diikuti dengan class exception nya.
+    // Jika method tersebut bisa menimnbulkan lebih dari satu jenis exception, kita bisa menambah lebih dari dari satu class exception.
+    // Di dalam kode program kita, untuk membuat exception kita cukup menggunakan kata kunci throw, diikuti dengan object exception nya.
+
+
+    public static void validate(LoginRequest loginRequest) throws ValidationException {
+        if (loginRequest.username() == null) {
+            throw new ValidationException("Username tidak boleh null");
+        } else if (loginRequest.username().isBlank()) {
+            throw new ValidationException("Username tidak boleh kosong");
+        } else if (loginRequest.password() == null) {
+            throw new ValidationException("Password tidak boleh null");
+        } else if (loginRequest.password().isBlank()) {
+            throw new ValidationException("Password tidak boleh kosong");
+        }
+    }
+
 }
