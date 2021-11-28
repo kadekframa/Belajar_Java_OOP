3d5e36144b11aae0ab3e9777f8696bb934915e5b
kadekframa <dframa19@gmail.com>
2021-11-28 21:56:03 +0800

    Java OOP Trainning Code, learn about Try With Resource in Java. Keep spirit dan Tetap Semangat meraih impian !

---
+++ README.md
@@ -0,0 +1,5 @@
+# Belajar Java Dasar
+
+Ini adalah project belajar Java Dasar di channel  :
+
+Programmer Zaman Now
---
+++ src/programmer/pemula/application/ReadApp.java
@@ -0,0 +1,12 @@
+package programmer.pemula.application;
+
+public class ReadApp {
+    public static void main(String[] args) {
+        // Try With Resource.
+        // Di Java 7, terdapat fitur baru yang bernama try with resource
+        // Try with resource adalah sebuah mekanisme agar kita lebih mudah menggunakan resource (yang wajib di close) dalam block try.
+        // Jika kita ingin mengunakan fitur ini, kita wajib menggunakan interface AutoCloseable.
+
+
+    }
+}
